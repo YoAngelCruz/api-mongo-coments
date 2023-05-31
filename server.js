f6d1d5319f8fd0abeeb3ee226d1e099003d85a2e
YoAngelCruz <angelcruz.feb@gmail.com>
2023-05-31 16:56:20 -0600

    create api mongo

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes/routes');
+
+const app = express();
+const port = 8080;
+app.use(cors());
+
+// Conexión a la base de datos MongoDB Atlas
+mongoose.connect('mongodb+srv://yoangelcruz:Ccam0810.@nosql.ukb8ydb.mongodb.net/?retryWrites=true&w=majority',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: 'nosql' // Reemplaza con el nombre real de tu base de datos
+});
+
+// Middleware para analizar datos JSON en las solicitudes
+app.use(express.json());
+app.use('/api', routes);
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
